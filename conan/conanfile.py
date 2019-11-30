b5f2423d8203229107469f0dcef0262bfa6750aa
sylsit <sitkowskis@interia.pl>
2019-11-30 08:49:48 +0100

    refactor. add new config CMakeList and change folder structure similar to template

---
+++ conan/conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class HelloConan(ConanFile):
+    name            = "packageTemplate"
+    version         = "0.1"
+    license         = "<Put the package license here>"
+    url             = "https://github.com/ssitkowx/PackageTemplate"
+    description     = "Package template"
+    settings        = "os", "compiler", "build_type", "arch"
+    options         = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators      = "cmake"
+    exports_sources = "source/*"
+    
+    def source(self):
+        cloneCmd = 'git clone ' + self.url + '.git'
+        self.run(cloneCmd)
+
+    def build(self):
+        currentPath = os.getcwd().replace('\conan','')
+        buildPath   = currentPath + '\\build'
+        cmake       = CMake(self)
+        cmake.configure(source_dir=currentPath, build_dir=buildPath)
+        cmake.build()
+        
+    def package(self):
+        self.copy('*.h'     , dst='include', src='../Project/include', keep_path=False)
+        self.copy('*.hxx'   , dst='include', src='../Project/include', keep_path=False)
+        self.copy('*.lib'   , dst='lib'    , src='../build/lib'      , keep_path=False)
+        self.copy('*.dll'   , dst='bin'    , src='../build/bin'      , keep_path=False)
+        self.copy('*.dylib*', dst='lib'    , src='../build/lib'      , keep_path=False)
+        self.copy('*.so'    , dst='lib'    , src='../build/lib'      , keep_path=False)
+        self.copy('*.a'     , dst='lib'    , src='../build/lib'      , keep_path=False)
+
+    #def package_info(self):
+        #self.cpp_info.libs = ["PackageTemplate"]
